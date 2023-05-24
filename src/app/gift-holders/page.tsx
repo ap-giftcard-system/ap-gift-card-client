cfd9af328a473440ac3efe05f92e2ff783b6dee4
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-24 18:58:03 -0500

    feat: created gift-holders route

---
+++ src/app/gift-holders/page.tsx
@@ -0,0 +1,9 @@
+const GiftHoldersPage = () => {
+  return (
+    <h1 className='head_text text-center'>
+      <span className='orange_gradient text-center'> Holders Page </span>
+    </h1>
+  );
+};
+
+export default GiftHoldersPage;
