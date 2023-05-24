7da27bf977fefef8fcb1cbe928371549ce7ffdf0
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-24 18:57:47 -0500

    feat: created sell route

---
+++ src/app/sell/page.tsx
@@ -0,0 +1,9 @@
+const SellPage = () => {
+  return (
+    <h1 className='head_text text-center'>
+      <span className='orange_gradient text-center'> Sell Page </span>
+    </h1>
+  );
+};
+
+export default SellPage;
