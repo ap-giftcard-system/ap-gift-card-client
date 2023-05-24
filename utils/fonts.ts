af00f2f1bf4294365f2616c76d8bc0d055022707
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-24 17:24:18 -0500

    feat: created fonts.ts

---
+++ utils/fonts.ts
@@ -0,0 +1,13 @@
+// @imports
+import { Nunito } from 'next/font/google';
+
+/** @dev define NunitoFonts */
+const NunitoFonts = Nunito({
+  weight: ['200', '300', '400', '600', '700', '800', '900'],
+  variable: '--font-nunito',
+  subsets: ['vietnamese'],
+  display: 'swap',
+});
+
+// @exports
+export default NunitoFonts;
