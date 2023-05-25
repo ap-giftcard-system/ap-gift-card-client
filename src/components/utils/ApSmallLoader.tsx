51a5f9a9889bf7adc09467d82459329ec7e57066
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-25 12:30:38 -0500

    feat: created ApSmallLoader component

---
+++ src/components/utils/ApSmallLoader.tsx
@@ -0,0 +1,9 @@
+const ApSmallLoader = () => {
+  return (
+    <div
+      className={`animate-spin rounded-full h-5 w-5 border-b-2 border-amber-500`}
+    />
+  );
+};
+
+export default ApSmallLoader;
