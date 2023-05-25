2cb01c57c720dceeb0059930057f727be54a4023
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-25 18:59:08 -0500

    feat: created new registerGiftHolder API

---
+++ src/api/gift-api.ts
@@ -0,0 +1,26 @@
+import { OFFICIAL_AP_BACKEND_BASEPATH } from '@/utils/constants';
+import { ApBackendError, ApGiftHolder } from '@/utils/interfaces';
+
+export const registerGiftHolder = async (apGiftHolder: ApGiftHolder) => {
+  try {
+    // prepare post body
+    const body = {
+      barcode: apGiftHolder.barCode,
+      holderName: apGiftHolder.holderName,
+      holderPhone: '+1' + apGiftHolder.holderPhone.replace(/\D/g, ''),
+      holderEmail: apGiftHolder.holderEmail,
+      giftAmount: Number(apGiftHolder.giftAmount),
+    };
+
+    // fetch registerAPI in the backend
+    const res = await fetch(
+      `${OFFICIAL_AP_BACKEND_BASEPATH}/gift/holder/register`,
+      { method: 'POST', body: JSON.stringify(body) }
+    );
+
+    return (await res.json()) as ApBackendError;
+  } catch (error) {
+    console.error(error);
+    return { error } as ApBackendError;
+  }
+};
