6e8fc3d74b60add1b0c33885d4c64dc367e4bf84
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-24 21:45:05 -0500

    feat: added ApGiftHolder global interface

---
+++ src/utils/interfaces.d.ts
@@ -0,0 +1,29 @@
+/**
+ * @notice interface contains information related to Ap Gift Holder
+ *
+ * @param giftHolderId - string
+ *
+ * @param barCode - string
+ *
+ * @param holderName - string
+ *
+ * @param holderPhone - string
+ *
+ * @param holderEmail - string
+ *
+ * @param giftAmount - number
+ *
+ * @param createdAt - string
+ *
+ * @param updatedAt - string
+ */
+export interface ApGiftHolder {
+  giftHolderId: string;
+  barCode: string;
+  holderName: string;
+  holderPhone: string;
+  holderEmail: string;
+  giftAmount: number;
+  createdAt: string;
+  updatedAt: string;
+}
