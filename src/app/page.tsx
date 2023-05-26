f86d30b342d3a4a018e8372b9a707853d99a22b3
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-25 23:04:52 -0500

    update: added new LoginForm component to root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import Image from 'next/image';
+import LoginForm from '@/components/LoginForm';
 
 export default function Home() {
   return (
-    <section className='w-full h-[100vh] flex justify-center items-center'>
+    <section className='w-full h-[100vh] flex flex-col pt-20 gap-9 sm:pt-32 sm:gap-16 items-center'>
       <h1 className='head_text text-center'>
         <span className='orange_gradient text-center'> AP Nail Art </span>
         <br className='max-md:hidden' />
         <p className='text-2xl sm:text-3xl'>Gift Card System</p>
       </h1>
+
+      {/* Login Form */}
+      <LoginForm />
     </section>
   );
 }
