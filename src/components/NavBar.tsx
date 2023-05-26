bcef5b58afb0e988b5c7ebb30db4633cb378c268
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-25 23:55:30 -0500

    refactor: refactored NavLinks to a client component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link';
-import { NAV_LINKS } from '@/utils/constants';
+import NavLinks from './utils/NavLink';
 
 const NavBar = () => {
   return (
     <nav
-      className='w-full mt-3 flex gap-3 justify-between items-center text-base px-3
+      className='w-full mt-3 flex justify-between items-center text-base px-3
                 sm:mt-0 sm:h-12 sm:gap-0 sm:text-lg sm:px-6
                 md:text-xl md:px-9'
     >
@@ -15,18 +15,9 @@ const NavBar = () => {
           AP Nail Art{' '}
         </span>
       </Link>
-      {/* navigations */}
-      <div className='flex gap-1 sm:gap-3 md:gap-9 tracking-tight'>
-        {NAV_LINKS.map((item) => (
-          <Link
-            key={item.title}
-            className='w-16 sm:w-28 border-1 transition duration-200 border-transparent hover:border-amber-500 flex justify-center rounded-lg hover:text-amber-500'
-            href={item.url}
-          >
-            {item.title}
-          </Link>
-        ))}
-      </div>
+
+      {/* navigation links*/}
+      <NavLinks />
     </nav>
   );
 };
---
+++ src/components/utils/NavLink.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import Link from 'next/link';
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
+import { NAV_LINKS } from '@/utils/constants';
+import { useRouter } from 'next/navigation';
+
+const NavLinks = () => {
+  const router = useRouter();
+  const [isAuth, setIsAuth] = useState(false);
+  const isAuthGranted = Cookies.get('_isAuth');
+
+  const handleLogOut = () => {
+    Cookies.remove('SESSIONID');
+    Cookies.remove('_isAuth');
+    router.push('/');
+    router.refresh();
+  };
+
+  useEffect(() => {
+    if (isAuthGranted) {
+      setIsAuth(true);
+    } else {
+      setIsAuth(false);
+    }
+  }, [isAuthGranted]);
+  return (
+    <>
+      {isAuth && (
+        <div className='flex gap-1 sm:gap-3 md:gap-9 tracking-tight'>
+          {NAV_LINKS.map((item) => (
+            <Link
+              key={item.title}
+              className='w-16 sm:w-24 border-1 transition duration-200 border-transparent hover:border-amber-500 flex justify-center rounded-lg hover:text-amber-500'
+              href={item.url}
+            >
+              {item.title}
+            </Link>
+          ))}
+          <button
+            onClick={handleLogOut}
+            className='w-16 sm:w-24 border-1 transition duration-200 border-transparent hover:border-red-500 flex justify-center rounded-lg hover:text-red-500'
+          >
+            Log out
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default NavLinks;
