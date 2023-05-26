e361483ae65505756e38316fbcc6c37dd9dbf741
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-26 13:30:06 -0500

    update: added authToken to registerGiftHolder

--- src/api/gift-api.ts
+++ src/api/gift-api.ts
@@ -1,8 +1,12 @@
 import { OFFICIAL_AP_BACKEND_BASEPATH } from '@/utils/constants';
 import { ApBackendObject, ApGiftHolder } from '@/utils/interfaces';
+import Cookies from 'js-cookie';
 
 export const registerGiftHolder = async (apGiftHolder: ApGiftHolder) => {
   try {
+    // prepare authToken
+    const authToken = Cookies.get('SESSIONID');
+
     // prepare post body
     const body = {
       barcode: apGiftHolder.barCode,
@@ -15,7 +19,13 @@ export const registerGiftHolder = async (apGiftHolder: ApGiftHolder) => {
     // fetch registerAPI in the backend
     const res = await fetch(
       `${OFFICIAL_AP_BACKEND_BASEPATH}/gift/holder/register`,
-      { method: 'POST', body: JSON.stringify(body) }
+      {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      }
     );
 
     return (await res.json()) as ApBackendObject;
