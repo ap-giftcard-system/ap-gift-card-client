8ad3dd6b71c72654154cbb63b11140801e29ff32
Logan Nguyen <lg.131.dev@gmail.com>
2023-05-28 19:31:16 -0500

    feat: added updateApGiftHolder API

--- src/api/holders-api.ts
+++ src/api/holders-api.ts
@@ -1,9 +1,11 @@
 import { OFFICIAL_AP_BACKEND_BASEPATH } from '@/utils/constants';
-import { ApBackendObject } from '@/utils/interfaces';
+import { ApBackendObject, ApGiftHolder } from '@/utils/interfaces';
 import Cookies from 'js-cookie';
 
 /**
  * @dev get a list of all holders
+ *
+ * @param barcode?: string - if none => retrieve all else retrieve single
  */
 export const getApGiftHolders = async (barcode?: string) => {
   try {
@@ -19,6 +21,35 @@ export const getApGiftHolders = async (barcode?: string) => {
         headers: {
           Authorization: `Bearer ${authToken}`,
         },
+      }
+    );
+
+    return (await res.json()) as ApBackendObject;
+  } catch (error) {
+    console.error(error);
+    return { error } as ApBackendObject;
+  }
+};
+
+/**
+ * @dev update gift holder
+ *
+ * @param updatedHolder ApGiftHolder
+ */
+export const updateApGiftHolder = async (updatedHolder: ApGiftHolder) => {
+  try {
+    // prepare auth token
+    const authToken = Cookies.get('SESSIONID');
+
+    // fetch find-gift-holders API
+    const res = await fetch(
+      `${OFFICIAL_AP_BACKEND_BASEPATH}/gift/holder/update`,
+      {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+        body: JSON.stringify(updatedHolder),
+        method: 'PATCH',
       }
     );
 
